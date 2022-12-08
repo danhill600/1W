2eb0c93d7e3299880a51e6a551fa23a0c6978e30
Dan Hill <dhill@witsquash.com>
2022-12-07 18:12:18 -0600

    removed commented-out experiments

--- Code.js
+++ Code.js
@@ -52,8 +52,6 @@ function Otterize () {
     var in_cn = anotherjson_data.callNumber;
     var loc = new locCallClass;
     var out_cn = loc.returnNormLcCall(in_cn);
-    //e.range.offset(0,1).setValue(result);
-    //e.range.offset(0,1)setValue(JSON.parse(result.getContextText()));
     if(json_data) {
       SpreadsheetApp.getActiveSheet().getRange('C' + row).setValue(in_cn);
       SpreadsheetApp.getActiveSheet().getRange('D' + row).setValue(out_cn)
@@ -72,37 +70,4 @@ function Otterize () {
    row++
   
   }//endforloop
-  //console.log(json_data.entries.length);
-    
-    /*
-    iterator = 2
-    json_data.entries.forEach(obj => {
-        Object.entries(obj).forEach(([key, value]) => {
-            if (key =='id'){
-              istring = 'A' + iterator
-              var itemID = `${value}`
-              SpreadsheetApp.getActiveSheet().getRange(istring).setValue(itemID);
-              
-               var url = 'https://librarycatalog2.ccc.edu/iii/sierra-api/v5/items/' + itemID;
-               /*
-               var options = {
-                 "method" : "GET",
-                 "headers" : {
-                  "Authorization" : "Bearer " + accesstoken
-                  }
-               };
-              var result = UrlFetchApp.fetch(url, options);
-              var json_data = JSON.parse(result.getContentText());
-              
-              var in_cn = json_data.callNumber;
-              var loc = new locCallClass;
-              var out_cn = loc.returnNormLcCall(in_cn);
-
-              SpreadsheetApp.getActiveSheet().getRange('D' + iterator).setValue(in_cn);
-              SpreadsheetApp.getActiveSheet().getRange('E' + iterator).setValue(out_cn);
-              iterator++;
-              //console.log(`${value}`);
-            }//endif
-        });
-    });*/
 }//end Otterize
