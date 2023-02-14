9e9a756188c4b150f79d9e55338a52b320628c48
Dan Hill <dhill@witsquash.com>
2023-02-13 18:48:24 -0600

    collect barcodes for inventory on sheet 1

--- Code.js
+++ Code.js
@@ -5,7 +5,7 @@ function Otterize() {
     var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
     var menuEntries = [];
 
-    menuEntries.push({name: "Get ItemIds in Range", functionName: "Cauterize"});
+    menuEntries.push({name: "Input Range Manually", functionName: "Cauterize"});
     menuEntries.push({name: "Get info for ItemIds", functionName: "getInfo"});
     spreadsheet.addMenu("Inventory", menuEntries);
     url = "https://librarycatalog2.ccc.edu/iii/sierra-api/v5/token";
@@ -27,9 +27,83 @@ function Otterize() {
 
 }//end Otterize
 
+function onOddit(e) {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  // cell must have a value, be only one row, and be from the first sheet
+  if( e.range.getValue() && (e.range.getNumRows() == 1) && sheet.getIndex() == 1 && e.range.getRow != 1) {
+
+
+  var accesstoken = scriptProperties.getProperty('accesstoken');
+  var value = e.range.getValue();
+  //populate column B w 'buhbhingo'
+  //e.range.offset(0,1).setValue(accesstoken);
+
+  var url = 'https://librarycatalog2.ccc.edu:443/iii/sierra-api/v6/items/query?offset=0&limit=1';
+
+  var options = {
+   "method" : "POST",
+   "headers" : {
+       "Authorization" : "Bearer " + accesstoken
+     },
+   "contentType" : "raw",
+   "payload" : '{"target":{"record":{"type":"item"},"field":{"tag":"b"}},"expr":{"op":"equals","operands":["' + value + '",""]}}'
+  };
+
+  var result = UrlFetchApp.fetch(url, options);
+  var json_data = JSON.parse(result.getContentText());
+//    e.range.offset(0,1).setValue(json_data)
+  //make sure we have data back ...
+  if(json_data) {
+    var entries = JSON.stringify(json_data.entries);
+
+    var itemID = entries.split('/')[7].split("\"")[0];
+//    e.range.offset(0,1).setValue(itemID);
+
+    var url = 'https://librarycatalog2.ccc.edu/iii/sierra-api/v5/items/' + itemID;
+
+    var options = {
+   "method" : "GET",
+   "headers" : {
+       "Authorization" : "Bearer " + accesstoken
+     }
+    };
+    var result = UrlFetchApp.fetch(url, options);
+    var json_data = JSON.parse(result.getContentText());
+    var in_cn = json_data.callNumber;
+    var loc = new locCallClass;
+    var out_cn = loc.returnNormLcCall(in_cn);
+    //e.range.offset(0,1).setValue(result);
+    //e.range.offset(0,1)setValue(JSON.parse(result.getContextText()));
+    if(json_data) {
+      e.range.offset(0,3).setValue(in_cn);
+      e.range.offset(0,4).setValue(out_cn)
+      e.range.offset(0,5).setValue(json_data.status.display);
+      e.range.offset(0,6).setValue(json_data.location.code);
+      e.range.offset(0,7).setValue('=\"' + Utilities.formatDate(new Date(), "GMT-4:00", "yyyy-MM-dd' 'HH:mm:ss") + '\"')
+      //e.range.offset(0,6).setValue(json_data.bibIds[0])
+
+      var bibId = json_data.bibIds[0];
+      var url = 'https://librarycatalog2.ccc.edu/iii/sierra-api/v5/bibs/' + bibId;
+      var result = UrlFetchApp.fetch(url, options);
+      var json_data = JSON.parse(result.getContentText());
+      if(json_data) {
+        e.range.offset(0,1).setValue(json_data.title);
+        e.range.offset(0,2).setValue(json_data.author);
+      }//end3rdif
+    }//end2ndif
+  }//end1stif
+  else{
+    e.range.offset(0,1).setValue('buhbingo');
+    }
+}//end onOddit
+}//endrow if
+
 function Cauterize() { // puts itemID's in a range into column H
   var accesstoken = scriptProperties.getProperty('accesstoken');
+
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  if(!(spreadsheet.getActiveSheet().getName()==='shelflist')) {
+    SpreadsheetApp.setActiveSheet(spreadsheet.getSheetByName('shelflist'))
 
   var starting = Browser.inputBox("Starting Call Number");
   var ending = Browser.inputBox("Ending Call Number");
@@ -56,12 +130,19 @@ function Cauterize() { // puts itemID's in a range into column H
 
 function getInfo() {
   var accesstoken = scriptProperties.getProperty('accesstoken');
+
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  if(!(spreadsheet.getActiveSheet().getName()==='shelflist')) {
+    SpreadsheetApp.setActiveSheet(spreadsheet.getSheetByName('shelflist'))
+    console.log("bingo");
+  }
+
   var lr=spreadsheet.getLastRow()
 
   for (i=2; i<lr; i++){
     if (( spreadsheet.getRange('A'+i).isBlank() ) && (i != 2)){
       var firstBlank = i-1;
+      console.log(firstBlank)
       break;
     }//end if
   }// end for loop
